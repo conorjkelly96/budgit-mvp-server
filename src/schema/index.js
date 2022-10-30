b584e9836caf85ac49066d0e5a1f4e557cee2a1c
Conor Kelly <conorjkelly96@gmail.com>
2022-10-30 13:57:01 +0000

    typedefs

---
+++ src/schema/index.js
@@ -0,0 +1,42 @@
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+  scalar Date
+
+  type User {
+    id: ID!
+    firstName: String!
+    lastName: String!
+    username: String!
+    email: String!
+    budgets: [Budget]
+  }
+
+  type Budget {
+    id: ID
+    name: String!
+    Salary: Float!
+    Other: Float!
+    Rentmortgage: Float!
+    Gym: Float!
+    Groceries: Float!
+    Housebills: Float!
+    Creditcard: Float!
+    Phone: Float!
+    Subscriptions: Float!
+    Holidaycost: Float!
+    Savings: Float!
+    Enjoymentfund: Float!
+  }
+  
+  # QUERIES
+  type Query {
+
+  }
+  # MUTATIONS
+  type Mutation {
+    
+  }
+`;
+
+module.exports = typeDefs;
