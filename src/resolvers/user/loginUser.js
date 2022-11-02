1bb648e2b6644f754f01567b84c10aa240d219b1
Conor Kelly <conorjkelly96@gmail.com>
2022-11-02 19:21:46 +0000

    loginuser function

---
+++ src/resolvers/user/loginUser.js
@@ -0,0 +1,39 @@
+const { AuthenticationError } = require("apollo-server");
+
+const { User } = require("../../models");
+const { signToken } = require("../../utils");
+
+const loginUser = async (_, { input }, context) => {
+  try {
+    const user = await User.findOne({ email: input.email });
+
+    if (!user) {
+      console.log("[ERROR]: Failed to login | User does not exist");
+      throw new AuthenticationError("Failed to login");
+    }
+
+    const isValidPassword = await user.checkPassword(input.password);
+
+    if (!isValidPassword) {
+      console.log("[ERROR]: Failed to login | Incorrect password");
+      throw new AuthenticationError("Failed to login");
+    }
+
+    return {
+      token: signToken(user),
+      user: {
+        id: user.id,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email,
+        username: user.username,
+        type: "user",
+      },
+    };
+  } catch (error) {
+    console.log(`[ERROR]: Failed to login | ${error.message}`);
+    throw new AuthenticationError("Failed to login");
+  }
+};
+
+module.exports = loginUser;
