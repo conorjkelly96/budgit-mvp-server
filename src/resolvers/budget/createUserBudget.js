8d164a41dcdf57dcc989233013e17fe2fd914eb8
Conor Kelly <conorjkelly96@gmail.com>
2022-11-08 19:18:05 +0000

    createUserBudget

---
+++ src/resolvers/budget/createUserBudget.js
@@ -0,0 +1,24 @@
+const { ApolloError, AuthenticationError } = require("apollo-server");
+
+const { Budget } = require("../../models");
+
+const { notAuthorized } = require("../../utils/errorMessage");
+
+const createItem = async (_, { input }, { user }) => {
+  try {
+    if (user) {
+      const user = user.id;
+      const newBudget = await Budget.create({ ...input, user });
+      const budget = newBudget.populate("user");
+      return budget;
+    } else {
+      console.log(`[ERROR]: Failed to create item | ${notAuthorized}`);
+      throw new AuthenticationError(notAuthorized);
+    }
+  } catch (error) {
+    console.log(`[ERROR]: Failed to create Budget | ${error.message}`);
+    throw new ApolloError("Failed to create Budget");
+  }
+};
+
+module.exports = createItem;
